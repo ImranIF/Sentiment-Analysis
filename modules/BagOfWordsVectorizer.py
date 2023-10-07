f676f7180bf2ac1f0e0e705568e04308fe83a24a
Imran Farid <faridimran1980@gmail.com>
2023-10-08 01:54:46 +0600

    Added feedback functionality. Proceed with caution

--- modules/BagOfWordsVectorizer.py
+++ modules/BagOfWordsVectorizer.py
@@ -1,5 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-# def vectorizeData(trainX, testX):
+from multipledispatch import dispatch
+
+@dispatch(list, list)
 def vectorizeData(trainX, testX):
     vectorizer = TfidfVectorizer()
     vectorizer.fit(trainX)
@@ -7,3 +9,10 @@ def vectorizeData(trainX, testX):
     testXVectors = vectorizer.transform(testX)
 
     return trainXVectors, testXVectors, vectorizer
+
+@dispatch(list)
+def vectorizeData(trainX):
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(trainX)
+    trainXVectors = vectorizer.transform(trainX)
+    return trainXVectors, vectorizer
