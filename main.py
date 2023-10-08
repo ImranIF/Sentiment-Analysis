04edacc6a287cf3c7f7f964a175a5245f5e436ec
Imran Farid <faridimran1980@gmail.com>
2023-10-08 13:43:09 +0600

    changed directories

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ st.sidebar.image('assets/IF-Logo3.png', caption='Natural Language Processing')
 page = st.sidebar.selectbox(label='Controller',
                             options=('Sentiment Analysis', 'Sentiment Feedback', 'Model Accuracy Comparison'))
 
-#STREAMLIT_STATIC_PATH = Path(st.__path__[0]) / 'static'
-#CSS_PATH = (STREAMLIT_STATIC_PATH / "static")
-#if not CSS_PATH.is_dir():
-#    CSS_PATH.mkdir()
+STREAMLIT_STATIC_PATH = Path(st.__path__[0]) / 'static'
+CSS_PATH = (STREAMLIT_STATIC_PATH / "static")
+if not CSS_PATH.is_dir():
+    CSS_PATH.mkdir()
 
-#css_file = CSS_PATH / "style.css"
-#if not css_file.exists():
-#    shutil.copy("static/style.css", css_file)
+css_file = CSS_PATH / "style.css"
+if not css_file.exists():
+    shutil.copy("css/style.css", css_file)
 st.markdown(
     f"""
-   <link rel="stylesheet" href="{st.static('style.css')}" type="text/css"/>
+   <link rel="stylesheet" href="css/style.css" type="text/css"/>
     """,
     unsafe_allow_html=True
 )
